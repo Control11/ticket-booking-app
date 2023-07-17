ed8f08fd0eb777bc22c9b834310672f8a9269e0a
Control11 <Control11@gmail.com>
2023-07-17 10:24:38 +0200

    create enums with ticket types and seat statuses

---
+++ src/main/java/com/ticket_booking_app/utils/SeatStatus.java
@@ -0,0 +1,8 @@
+package com.ticket_booking_app.utils;
+
+public enum SeatStatus {
+    AVAILABLE,
+    RESERVED,
+    BOUGHT
+
+}
---
+++ src/main/java/com/ticket_booking_app/utils/TicketType.java
@@ -0,0 +1,11 @@
+package com.ticket_booking_app.utils;
+
+import lombok.ToString;
+
+@ToString
+public enum TicketType {
+    ADULT,
+    CHILD,
+    STUDENT
+}
+
