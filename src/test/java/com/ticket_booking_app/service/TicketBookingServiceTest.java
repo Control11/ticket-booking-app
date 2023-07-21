19a9f32fbf3fe705d1b171ae885ca1a19df93989
Control11 <Control11@gmail.com>
2023-07-21 17:55:10 +0200

    service tests - initialize ideas for tests

---
+++ src/test/java/com/ticket_booking_app/service/TicketBookingServiceTest.java
@@ -0,0 +1,37 @@
+package com.ticket_booking_app.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TicketBookingServiceTest {
+
+    @Test
+    void shouldNotAllowsToBookAfterTimeLimit() {
+    }
+
+
+
+    @Test
+    void shouldValidateCustomerNameMinimalLength() {
+    }
+    @Test
+    void shouldValidateCustomerSurnameMinimalLength() {
+    }
+    @Test
+    void shouldNotValidateCustomerNameWhenStartsWithLowerCase() {
+    }
+    @Test
+    void shouldValidateCustomerSurnameWhenContainsDash() {
+    }
+    @Test
+    void shouldNotValidateCustomerSurnameWhenAfterDashStartsWithLowerCase() {
+    }
+
+
+
+
+    @Test
+    void shouldCreateReservation() {
+    }
+}
