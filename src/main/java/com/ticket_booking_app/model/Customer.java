b16d578fc621f374f74cc78c7a29957fac3f3fe2
Control11 <Control11@gmail.com>
2023-07-24 11:55:34 +0200

    add more demanding validation to the customer's name and surname

--- src/main/java/com/ticket_booking_app/model/Customer.java
+++ src/main/java/com/ticket_booking_app/model/Customer.java
@@ -24,20 +24,45 @@ public class Customer {
     @Length(min = 3, message = "Surname should be at least 3 characters long")
     private String surname;
 
-    @AssertTrue(message = "Name should starts with a capital letter")
+    @AssertTrue(message = "Name should start with a capital letter and rest of the letters should be lower case type")
     public boolean isNameValid() {
-        return Character.isUpperCase(name.charAt(0));
+        char[] nameLetters = name.toCharArray();
+        boolean isValid = false;
+
+        for (int i = 0; i < nameLetters.length; i++) {
+            if(i != 0){
+                isValid = Character.isLowerCase(nameLetters[i]);
+            }else {
+                isValid = Character.isUpperCase(nameLetters[i]);
+            }
+
+            if(!isValid){
+                return isValid;
+            }
+        }
+        return isValid;
     }
 
-    @AssertTrue(message = "Surname should starts with a capital letter")
+    @AssertTrue(message = "Surname should start with a capital letter and rest of the letters should be lower case type")
     public boolean isSurnameValid() {
-        if (Character.isUpperCase(surname.charAt(0))) {
-            if (surname.contains("_")) {
-                return Character.isUpperCase(surname.split("_")[1].charAt(0));
+        char[] surnameLetters = surname.toCharArray();
+        boolean isValid = false;
+
+        for (int i = 0; i < surnameLetters.length; i++) {
+            if(surnameLetters[i] == '_' && i !=0){
+                isValid = Character.isUpperCase(surnameLetters[++i]);
+            }else if(i != 0){
+                isValid = Character.isLowerCase(surnameLetters[i]);
+            }else {
+                isValid = Character.isUpperCase(surnameLetters[i]);
             }
-            return true;
+
+            if(!isValid){
+                return isValid;
+            }
         }
-        return false;
+
+        return isValid;
     }
 
 }
